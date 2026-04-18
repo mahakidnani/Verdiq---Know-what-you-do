e7f12554543bbb914ee3540adbe4d34dc6573e98
Mahak Idnani <mahakidnaniwork@gmail.com>
2026-04-18 06:25:24 +0100

    Create start.py

---
+++ start.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
+sys.path.insert(0, os.path.dirname(__file__))
+
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
